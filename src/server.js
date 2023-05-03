ca8afbd8bb0c1d54fb3ef2aae99caa65154413a2
Ryu0012 <seungjy10@gmail.com>
2023-05-03 18:31:49 +0900

    js base_ server

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import "./db";
+import "./models/Video";
+import express from "express";
+import morgan from "morgan";
+
+import globalRouter from "./routers/globalRouter";
+// import userRouter from "./routers/userRouter";
+
+const PORT = 5000;
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views/");
+const logger = morgan("dev");
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
+app.use("/", globalRouter);
+// app.use("/users", userRouter);
+
+const handleListening = () =>
+  console.log(`Server listenting on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
