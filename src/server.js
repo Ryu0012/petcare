cdfd197484301ceaf06428bc29da7c55a0bc0d5e
sanghee313 <cosmos.csh@gmail.com>
2023-05-08 11:47:35 +0900

    server.js 수정

--- src/server.js
+++ src/server.js
@@ -2,15 +2,23 @@
 import express from "express";
 import morgan from "morgan";
 
-import globalRouter from "./routers/globalRouter";
+// import globalRouter from "./routers/globalRouter";
 // import userRouter from "./routers/userRouter";
+const petsRouter = require("./routers/petsRouter.js");
+const loginRouter = require("./routers/loginRouter.js");
+const homeRouter = require("./routers/homeRouter.js");
+const familycareRouter = require("./routers/familycareRouter.js");
+const iotRouter = require("./routers/iotRouter.js");
 
 const PORT = 5000;
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", process.cwd() + "/src/views/");
+// app.set("view engine", "pug");
+// app.set("views", process.cwd() + "/src/views/");
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
 const logger = morgan("dev");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
