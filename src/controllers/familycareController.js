23de6b86d48f1590bbb6d85f5326c826a40a68b9
sanghee313 <cosmos.csh@gmail.com>
2023-05-21 23:48:42 +0900

    npm install mysql2 mybatis-mapper body-parser

--- src/controllers/familycareController.js
+++ src/controllers/familycareController.js
@@ -3,7 +3,7 @@ export const getFamilycare = (req, res) => {
 };
 module.exports.getFamilycare = getFamilycare;
 
-export const addFamilycare = (req, res) => {
-  return res.render("family/familyv2");
-};
-module.exports.addFamilycare = addFamilycare;
+// export const addFamilycare = (req, res) => {
+//   return res.render("family/familyv2");
+// };
+// module.exports.addFamilycare = addFamilycare;
--- src/routers/familyRouter.js
+++ src/routers/familyRouter.js
@@ -2,12 +2,12 @@ import express from "express";
 
 const familyRouter = express.Router();
 
-// import {getFamilycare, addFamilycare} from "../controllers/familycareController";
+import { getFamilycare } from "../controllers/familycareController";
 
 // 임의로 해뒀습니다. 컨트롤러 연결해서 작업하시면 됩니다.
-const handleFamily = (req, res) => res.send("calendar");
+// const handleFamily = (req, res) => res.send("calendar");
 
 // http://localhost:5000/family
-familyRouter.get("/", handleFamily);
+familyRouter.get("/", getFamilycare);
 
 export default familyRouter;
