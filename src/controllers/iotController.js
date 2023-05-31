2d8b51da5b5222f7c393fca38394a6766a8a1eee
Ryu0012 <seungjy10@gmail.com>
2023-05-31 20:34:02 +0900

    iot part, css

--- src/controllers/iotController.js
+++ src/controllers/iotController.js
@@ -1,3 +1,3 @@
 //iot 페이지
-export const iot = (req, res) => res.render("iot/iotCheck");
+export const iot = (req, res) => res.render("iot/view");
 export const edit = (req, res) => res.render("iot/iotEdit");
--- src/routers/iotRouter.js
+++ src/routers/iotRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { iot, edit } from "../controllers/iotController";
+import { iot } from "../controllers/iotController";
 
 //iot 관리 page
 const iotRouter = express.Router();
 
 iotRouter.get("/", iot);
-iotRouter.get("/edit", edit);
+// iotRouter.get("/edit", edit);
 
 export default iotRouter;
